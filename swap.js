65fbb5eb7bda45b98b5b9dc1b4a879a7edc9e3d0
nandu <192p1a04b5nandu@gmail.com>
2024-06-03 17:44:00 +0530

    added  new file

---
+++ swap.js
@@ -0,0 +1,10 @@
+// swapping of two variables
+
+let name1="kajal";
+let name2="anushkha";
+console.log("before swAPPING:",name1,name2)
+let name3;
+name3=name1;
+name1=name2;
+name2=name3;
+console.log("after swapping :",name1,name2);
